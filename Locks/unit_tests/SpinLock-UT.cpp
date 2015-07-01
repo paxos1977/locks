eda5e51e433b3fde44a7bf67fa9e307b6904931f
Austin Gilbert <ceretullis@gmail.com>
2015-06-30 21:12:22 -0500

    Fleshing out unit test for prooving correctness.

--- Locks/unit_tests/SpinLock-UT.cpp
+++ Locks/unit_tests/SpinLock-UT.cpp
@@ -1,7 +1,9 @@
 #include <UnitTest++/UnitTest++.h>
 #include <Locks/SpinLock.hpp>
 
+#include <cstddef>
 #include <mutex>
+#include <thread>
 
 namespace {
 
@@ -20,5 +22,28 @@ namespace {
     {
         locks::SpinLock lock;
         std::unique_lock<locks::SpinLock> guard(lock);
+    }
+
+    TEST(verifySpinLockProtectsSharedVariable)
+    {
+        locks::SpinLock lock;
+        std::size_t count = 0;
+
+        auto func = [&lock, &count](){
+
+            for(std::size_t i = 0; i < 100; ++i)
+            {
+                std::unique_lock<locks::SpinLock> guard(lock);
+                count++;
+            }
+        };
+
+        std::thread t1(func);
+        std::thread t2(func);
+
+        t1.join();
+        t2.join();
+
+        CHECK_EQUAL(200U, count);
     }
 }
